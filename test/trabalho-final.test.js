83cbbf707730458ecb6398a4e981168528acf6ec
Leonardo Machado Fernandes <leo.machandes@gmail.com>
2023-05-22 20:01:26 -0300

    Initial commit

---
+++ test/trabalho-final.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const TrabalhoFinal = require('../lib/trabalho-final-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/trabalho-final-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new TrabalhoFinal.TrabalhoFinalStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
