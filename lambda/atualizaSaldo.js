d2767302918c25d0663877ff9678f0c78894d3b3
Leonardo Machado Fernandes <leo.machandes@gmail.com>
2023-05-31 23:03:05 -0300

    Ajustes

--- lambda/atualizaSaldo.js
+++ lambda/atualizaSaldo.js
@@ -1,24 +1,111 @@
 const AWS = require('aws-sdk');
-const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async function(event, context) {
     console.log('Event: ', JSON.stringify(event, null, 2));
     for (const record of event.Records) {
         const messageBody = JSON.parse(record.body);
-        const id = messageBody.id;
-        const valor = messageBody.valor;
-        
+        const codigoUltimaTransacao = messageBody.id;
+        console.log("Body", codigoUltimaTransacao)
+
+            
         try {
-            const data = await dynamodb.put({
-                TableName: process.env.TABLE_NAME,
-                Item: {
-                    id:id,
-                    valor: valor
-                },
-            }).promise();
-            console.log('Saldo atualizado com sucesso!')
+            const conta = await buscalUltimaTransacao(codigoUltimaTransacao)
+           
+            const saldo = await buscaSaldoAtual(conta.idCliente);
+            console.log("Valor do saldo", saldo)
+            let valorSaldo = 0;
+            if (saldo != null) {
+                valorSaldo = saldo.valor;
+            }
+            if (conta.tipo == 'entrada') {
+                valorSaldo+=conta.valor;
+            } else {
+                valorSaldo-=conta.valor
+            }
+
+            const dbAtualizar = new AWS.DynamoDB.DocumentClient();
+            var dynamodb = new AWS.DynamoDB()
+            
+            if (saldo != null) {
+                const params = {
+                    TableName: process.env.TABLE_NAME,
+                    Key: {
+                        idCliente: { S: conta.idCliente.toString() }
+                    },
+                    UpdateExpression: 'SET valor = :valor',
+                    ExpressionAttributeValues: {
+                         ':valor': { N: valorSaldo.toString() }
+                    },
+                    ReturnValues: 'UPDATED_NEW'
+                  };
+    
+                  console.log('Valor: ', valorSaldo)
+
+                  await dynamodb.updateItem(params).promise();
+                  
+
+            } else {
+                await dbAtualizar.put({
+                    TableName: process.env.TABLE_NAME,
+                    Item: {
+                        idCliente:conta.idCliente.toString(),
+                        valor: valorSaldo
+                    },
+                }).promise();
+
+            }
+
+              console.log('Fim da atualização')
+              
         } catch (error) {
-            console.error(`Error inserting item into DynamoDB table: ${error}`);
+            console.error(`Erro os inserir dados na tabela saldo: ${error}  `);
         }
     }
+
+    async function buscalUltimaTransacao(codigoUltimaTransacao) {
+
+        try {
+
+            const docUltimaTransacao = new AWS.DynamoDB.DocumentClient();
+            let conta = null;
+            const data = await docUltimaTransacao.scan({
+                TableName:'Contas'
+            }).promise();
+            data.Items.forEach((item) => {
+                if (conta === null && item.id === codigoUltimaTransacao) {
+                    conta = item
+                }
+
+            });
+            return conta;
+        }catch (error) {
+          console.error("Erro ao consultar a conta:", error);
+          throw error;
+        }
+    }
+
+    async function buscaSaldoAtual(idCliente) {
+        try {
+            const docSaldo = new AWS.DynamoDB.DocumentClient();
+            let saldo = null;
+            const data = await docSaldo.scan({
+                TableName:'Saldo'
+            }).promise();
+            data.Items.forEach((item) => {
+                console.log("Saldo", saldo)
+                console.log(saldo === null)
+                console.log(item.idCliente == idCliente)
+                if (saldo === null && item.idCliente == idCliente) {
+                    saldo = item
+                }
+
+            });
+            return saldo;
+        }catch (error) {
+          console.error("Erro ao consultar a saldo atual:", error);
+          throw error;
+        }
+    }
+      
+      
 };
--- lambda/send-message-to-queue.js
+++ lambda/send-message-to-queue.js
@@ -3,11 +3,43 @@ const sqs = new AWS.SQS();
 
 exports.handler = async function(event, context) {
     console.log('Event: ', JSON.stringify(event, null, 2));
+
+    const idCliente = event.Records[0].dynamodb.NewImage.idCliente.N
+    let saldo = 0;
+    try {
+        const params = {
+            TableName: "Conta", 
+            FilterExpression: "#idCliente = :idCliente",
+            ExpressionAttributeNames: {
+              "#idCliente": "idCliente",
+            },
+            ExpressionAttributeValues: {
+              ":idCliente": { N: idCliente.toString() },
+            },
+          };
+
+        const command = new ScanCommand(params);
+        const response = await client.send(command)
+        response.Items.forEach((item) => {
+            const tipo = item.tipo.S;
+            const valor = Number(item.valor.N);
+      
+            if (tipo === "entrada") {
+              saldo += valor;
+            } else if (tipo === "saída") {
+              saldo -= valor;
+            }
+          });
+
+    }catch(error) {
+        console.error("Erro ao calcular o saldo: ", error)
+    }
+
     
     const params = {
         MessageBody: JSON.stringify({
             id: event.Records[0].dynamodb.Keys.id.S,
-            valor: parseFloat(event.Records[0].dynamodb.NewImage.valor.N)
+            valor: saldo
         }),
         QueueUrl: process.env.QUEUE_URL,
     };
