fcaba2d5a615fb5d3014d0877094ff4a9799877b
Leonardo Machado Fernandes <leo.machandes@gmail.com>
2023-05-31 23:54:53 -0300

    Ajustes

--- lambda/atualizaSaldo.js
+++ lambda/atualizaSaldo.js
@@ -5,14 +5,12 @@ exports.handler = async function(event, context) {
     for (const record of event.Records) {
         const messageBody = JSON.parse(record.body);
         const codigoUltimaTransacao = messageBody.id;
-        console.log("Body", codigoUltimaTransacao)
 
             
         try {
             const conta = await buscalUltimaTransacao(codigoUltimaTransacao)
            
             const saldo = await buscaSaldoAtual(conta.idCliente);
-            console.log("Valor do saldo", saldo)
             let valorSaldo = 0;
             if (saldo != null) {
                 valorSaldo = saldo.valor;
@@ -23,10 +21,10 @@ exports.handler = async function(event, context) {
                 valorSaldo-=conta.valor
             }
 
-            const dbAtualizar = new AWS.DynamoDB.DocumentClient();
-            var dynamodb = new AWS.DynamoDB()
+            
             
             if (saldo != null) {
+                var dynamodb = new AWS.DynamoDB()
                 const params = {
                     TableName: process.env.TABLE_NAME,
                     Key: {
@@ -45,7 +43,8 @@ exports.handler = async function(event, context) {
                   
 
             } else {
-                await dbAtualizar.put({
+                const dbCadastrar = new AWS.DynamoDB.DocumentClient();
+                await dbCadastrar.put({
                     TableName: process.env.TABLE_NAME,
                     Item: {
                         idCliente:conta.idCliente.toString(),
@@ -55,7 +54,6 @@ exports.handler = async function(event, context) {
 
             }
 
-              console.log('Fim da atualização')
               
         } catch (error) {
             console.error(`Erro os inserir dados na tabela saldo: ${error}  `);
@@ -65,46 +63,36 @@ exports.handler = async function(event, context) {
     async function buscalUltimaTransacao(codigoUltimaTransacao) {
 
         try {
-
             const docUltimaTransacao = new AWS.DynamoDB.DocumentClient();
-            let conta = null;
-            const data = await docUltimaTransacao.scan({
-                TableName:'Contas'
-            }).promise();
-            data.Items.forEach((item) => {
-                if (conta === null && item.id === codigoUltimaTransacao) {
-                    conta = item
-                }
-
-            });
+            const data = await docUltimaTransacao
+              .scan({ TableName: 'Contas' })
+              .promise();
+        
+            const conta = data.Items.find((item) => item.id === codigoUltimaTransacao);
+        
+            if (!conta) {
+              throw new Error('Conta não encontrada');
+            }
+        
             return conta;
-        }catch (error) {
-          console.error("Erro ao consultar a conta:", error);
-          throw error;
-        }
+          } catch (error) {
+            console.error('Erro ao consultar a conta:', error);
+            throw error;
+          }
     }
 
     async function buscaSaldoAtual(idCliente) {
         try {
             const docSaldo = new AWS.DynamoDB.DocumentClient();
-            let saldo = null;
-            const data = await docSaldo.scan({
-                TableName:'Saldo'
-            }).promise();
-            data.Items.forEach((item) => {
-                console.log("Saldo", saldo)
-                console.log(saldo === null)
-                console.log(item.idCliente == idCliente)
-                if (saldo === null && item.idCliente == idCliente) {
-                    saldo = item
-                }
-
-            });
-            return saldo;
-        }catch (error) {
-          console.error("Erro ao consultar a saldo atual:", error);
-          throw error;
-        }
+            const data = await docSaldo.scan({ TableName: 'Saldo' }).promise();
+        
+            const saldo = data.Items.find((item) => item.idCliente == idCliente);
+        
+            return saldo || null;
+          } catch (error) {
+            console.error('Erro ao consultar o saldo atual:', error);
+            throw error;
+          }
     }
       
       
