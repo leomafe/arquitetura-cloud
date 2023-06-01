684b35e0d543c5a88f229b508ffc464e1540102f
Leonardo Machado Fernandes <leo.machandes@gmail.com>
2023-06-01 17:16:57 -0300

    Ajuste e adicionado parametro a consulta do saldo

--- lambda/buscaSaldo.js
+++ lambda/buscaSaldo.js
@@ -2,9 +2,14 @@ const { DynamoDB } = require('aws-sdk');
 
 exports.handler = async function(event, context) {
     try {
+        const parametro = event.pathParameters.parametro;
         const docClient = new DynamoDB.DocumentClient();
         const data = await docClient.scan({
             TableName: 'Saldo',
+            FilterExpression: 'idCliente = :param',
+            ExpressionAttributeValues: {
+                ':param': parametro
+            }
         }).promise();
         return {
             statusCode: 200,
